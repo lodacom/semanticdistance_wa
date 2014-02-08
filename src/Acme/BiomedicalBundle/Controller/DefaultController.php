831509f3e70f28aab9bfcfb154115ecc98a57c33
Duplouy Olivier <olivier.duplouy@etud.univ-montp2.fr>
2014-02-08 11:22:58 +0100

    Rajout d'un champ pour faciliter la requête (Service 1)

--- src/Acme/BiomedicalBundle/Controller/DefaultController.php
+++ src/Acme/BiomedicalBundle/Controller/DefaultController.php
@@ -31,18 +31,25 @@ class DefaultController extends Controller
             $term = $request->request->get('search');
             
             $quer = $this->getDoctrine()->getEntityManager();
-            $query=$quer->createQuery("SELECT t.name as term,o.name as ontology
+            /*$query=$quer->createQuery("SELECT t.name as term,o.name as ontology
             		FROM AcmeBiomedicalBundle:Term t, AcmeBiomedicalBundle:Concept c,AcmeBiomedicalBundle:Ontology o
             		WHERE t.name LIKE ?1
             		AND t.concept_id=c.id
             		AND c.ontology_id=o.id
             		ORDER BY t.name ASC")
             ->setParameter(1,$term."%")
-            ->setMaxResults(10);
+            ->setMaxResults(10);*/
+            $query=$quer->createQuery("SELECT t.name as term
+            		FROM AcmeBiomedicalBundle:Term t
+            		WHERE t.name LIKE ?1
+            		ORDER BY t.name ASC")
+                        		->setParameter(1,$term."%")
+                        		->setMaxResults(10);
             $recup = $query->getArrayResult();
             $concepts=array();
 			foreach ( $recup as $data ) {
-				$concepts[] = $data ['term']." (".$data ['ontology'].")";
+				//$concepts[] = $data ['term']." (".$data ['ontology'].")";
+				$concepts[] = $data ['term'];
 			}
 			if (count($recup)==0){
 				array_push($concepts, "Aucune proposition");
@@ -53,19 +60,52 @@ class DefaultController extends Controller
         }
     }
     
-    public function showOntologiesAction(){
+    public function showOntologiesWithConceptAction(){
     	$request = $this->get('request');
     	 
     	if($request->isXmlHttpRequest()){
-    		$term = $request->request->get('searchOntology');
+    		$term_ontology = $request->request->get('search');
+    		$tab=split("/",$term_ontology);
+    		$term=$tab[0];
+    		$ontology=$tab[1];
+    		
+    		$quer = $this->getDoctrine()->getEntityManager();
+    		$query=$quer->createQuery("SELECT o.name
+            		FROM AcmeBiomedicalBundle:Ontology o,AcmeBiomedicalBundle:Term t,AcmeBiomedicalBundle:Concept c
+            		WHERE o.name LIKE ?1
+    				AND t.name=?2
+    				AND t.concept_id=c.id
+    				AND c.ontology_id=o.id
+    				ORDER BY o.name ASC")
+    	            		->setParameters(array(1=>"%".$ontology."%",2=>$term))
+    	            		->setMaxResults(10);
+    		$recup = $query->getArrayResult();
+    		$concepts=array();
+    		foreach ( $recup as $data ) {
+    			$concepts[] = $data ['name'];
+    		}
+    		if (count($recup)==0){
+    			array_push($concepts, "Aucune proposition");
+    		}
+    		$response = new Response(json_encode($concepts));
+    		$response->headers->set('Content-Type', 'application/json');
+    		return $response;
+    	}
+    }
+    
+    public function showOntologiesAction(){
+    	$request = $this->get('request');
     	
+    	if($request->isXmlHttpRequest()){
+    		$term = $request->request->get('searchOntology');
+    		 
     		$quer = $this->getDoctrine()->getEntityManager();
     		$query=$quer->createQuery("SELECT o.name
             		FROM AcmeBiomedicalBundle:Ontology o
             		WHERE o.name LIKE ?1
     				ORDER BY o.name ASC")
-    	            		->setParameter(1,$term."%")
-    	            		->setMaxResults(10);
+    	    				->setParameter(1,$term."%")
+    	    				->setMaxResults(10);
     		$recup = $query->getArrayResult();
     		$concepts=array();
     		foreach ( $recup as $data ) {
